2724899c7c9bcdfab8a3ee8437dad24a6a564d3d
kcoulomb <kevin.coulomb@sysfera.com>
2013-11-05 14:25:57 +0100

    Script display usage now

--- generic/generic.sh
+++ generic/generic.sh
@@ -2,6 +2,17 @@
 TIME=0.5
 REP=$1
 clear;
+
+if [ $# -lt 1 ]; then
+    echo ""
+    echo "**********************************************"
+    echo "WRONG COMMAND. "
+    echo "Usage: " $0 " <opt>"
+    echo "with opt value in : kitty, bunny, duck"
+    echo "**********************************************"
+    exit 1
+fi
+
 while true 
   do
     for i in {1..12}; do more img_$REP/wall_$i; sleep $TIME; clear; done;
